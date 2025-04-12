e82b06df1efb34ad4c59d70c44cb3ac7c8f67edd
kevvj <kevinjvillalba774+1@gmail.com>
2025-04-11 22:14:51 -0500

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,35 @@
-import { Text, View } from 'react-native';
+import { useEffect } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function Home() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hola</Text>
-    </View>
-  )
+
+    const text = "Esto es un texto de prueba para separar en palabras"
+    const separatedText = text.split(" ")
+    const identifiedText = separatedText.map((word, index) => ({
+        id: index,
+        value: word
+    }))
+
+    const styles = StyleSheet.create({
+        container: {
+            display: 'flex', justifyContent: 'center', alignItems: 'center', flex: 1
+        },
+        title: {
+            flexDirection: 'row', flexWrap: 'wrap', gap: 7
+        },
+        parrafo: {
+            flexDirection: 'row', flexWrap: 'wrap', gap: 7
+        },
+    })
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.parrafo}>
+                {identifiedText.map((word) => (
+                    <Text key={word.id}>{word.value}</Text>
+                ))}
+            </View>
+
+        </View>
+    )
 }
